26fddca66e5f027e1d60c50f77877a45f5bb0efa
web-dev <nathankim137311@gmail.com>
2021-11-25 20:52:15 -0800

    added commands file to store shortcuts, commands and cheatsheets

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,11 @@
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
 const path = require('path');
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 module.exports = {
   entry: './src/js/script.js',
   output: {
     filename: 'main.js',
+    // eslint-disable-next-line no-undef
     path: path.resolve(__dirname, 'dist'),
   },
-  mode: 'development'
+  mode: 'production'
 };
